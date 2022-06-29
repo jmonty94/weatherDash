1b7e20fe11d7c2ac726f54f33eaedea02a4bd942
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-06-28 23:59:00 -0700

    created new elements for the cards and worked on calling the correct data for the icon and formatted date

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,15 @@
 // openWeather Variables
 const oneCallURL = 'https://api.openweathermap.org/data/3.0/onecall';
-const geocodingURL = 'http://api.openweathermap.org/geo/1.0/direct'
+const geocodingURL = 'http://api.openweathermap.org/geo/1.0/direct';
 const openWeatherAPIKey = `&appid=61eaae33c7b186d0a805e57d11aff3bb`;
+const iconUrl = `http://openweathermap.org/img/wn/`;
+const iconEnding = `@2x.png`;
 // const openWeatherAPIKey = `&appid=f606e9f4074125fb3ea381f91319ce19`;
 const latTag = `?lat=`;
 const lonTag = `&lon=`;
 const excludeTag = `&exclude=`;
 const unitTag = `&units=imperial`;
-
 const qTag = `?q=`;
-
 let exclude = `minutely,hourly,alerts`;
 
 // Html element variables
@@ -22,18 +22,13 @@ const currTempEl = $("#currTemp");
 const currWindEl = $("#currWind");
 const currHumidityEl = $("#currHumidity");
 const currUVIndexEl = $("#currUVIndex");
-const uvValEl = $("#uvVal")
-const forecastContainerEl = document.getElementById("forecastContainer")
-// const forecastContainerEl = $("#forecastContainer")
-
-// const fiveDayEl = document.getElementById("fiveDay")
-// navBar.append("<ul>Previous Searches</ul");
+const uvValEl = $("#uvVal");
+const forecastContainerEl = document.getElementById("forecastContainer");
 
 function search() {
     let city = searchInputEl.val();
     searchInputEl.val("");
     if (city !== "") {
-        navBarEl.addClass("col-3").removeClass("col-12");
         resultEl.removeClass("d-none");
         getLatLon(city);
     };
@@ -42,37 +37,37 @@ function getLatLon(city) {
     let lat;
     let lon;
     fetch(geocodingURL + qTag + city + openWeatherAPIKey)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        lat = data[0].lat;
-        lon = data[0].lon;
-        city = data[0].name;
-        state = data[0].state;
-        getCityWeather(lat, lon, city, state);
-    });
-};
-function getCityWeather(lat, lon, city, state){
-    fetch(oneCallURL + latTag + lat + lonTag + lon + openWeatherAPIKey + unitTag)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        fiveDay = data.daily.slice(1, 6)
-        console.log(fiveDay);
-
-        // console.log(forecastArray);
-        currentTemp = data.current.temp;
-        currentWind = data.current.wind_speed;
-        currentHumidity = data.current.humidity;
-        currentUVI = data.current.uvi;
-        generateCurrentWeather(city, state, currentTemp, currentWind, currentHumidity, currentUVI);
-        fiveDay.forEach(day =>  {
-            generateForecastCard(day)
+        .then(function (response) {
+            return response.json();
         })
-    });
+        .then(function (data) {
+            lat = data[0].lat;
+            lon = data[0].lon;
+            city = data[0].name;
+            state = data[0].state;
+            getCityWeather(lat, lon, city, state);
+        });
+};
+function getCityWeather(lat, lon, city, state) {
+    fetch(oneCallURL + latTag + lat + lonTag + lon + openWeatherAPIKey + unitTag)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            fiveDay = data.daily.slice(1, 6);
+            console.log(fiveDay);
+
+            // console.log(forecastArray);
+            currentTemp = data.current.temp;
+            currentWind = data.current.wind_speed;
+            currentHumidity = data.current.humidity;
+            currentUVI = data.current.uvi;
+            generateCurrentWeather(city, state, currentTemp, currentWind, currentHumidity, currentUVI);
+            fiveDay.forEach(day => {
+                generateForecastCard(day);
+            });
+        });
 };
 function generateCurrentWeather(city, state, currentTemp, currentWind, currentHumidity, currentUVI) {
     console.log(city, state, currentTemp, currentWind, currentHumidity, currentUVI);
@@ -80,28 +75,39 @@ function generateCurrentWeather(city, state, currentTemp, currentWind, currentHu
     currTempEl.text("Temp: " + currentTemp);
     currWindEl.text("Wind: " + currentWind);
     currHumidityEl.text("Humidity: " + currentHumidity);
-    uvValEl.before("UV Index: ")
-    uvValEl.text(currentUVI)
+    uvValEl.before("UV Index: ");
+    uvValEl.text(currentUVI);
     if (currentUVI <= 2) {
-        uvValEl.addClass("bg-success")
-    } else if (currentUVI > 2 && currentUVI <= 7){
-        uvValEl.addClass("bg-warning")
-    } else if (currentUVI > 7){
-        uvValEl.addClass("bg-danger")
-    }
-}
-function generateForecastCard(day){
+        uvValEl.addClass("bg-success");
+    } else if (currentUVI > 2 && currentUVI <= 7) {
+        uvValEl.addClass("bg-warning");
+    } else if (currentUVI > 7) {
+        uvValEl.addClass("bg-danger");
+    };
+};
+function generateForecastCard(day) {
     // const cardEL = document.createElement("div")
-    const cardEL = document.createElement("div")
-    const cardHeadingEl = document.createElement("h1")
-    cardEL.classList.add("card")
-    forecastContainerEl.appendChild(cardEL)
-    cardEL.appendChild(cardHeadingEl)
-    const formattedDate = moment.unix(day.dt).format("MM-DD-YYYY")
-    cardHeadingEl.textContent = formattedDate
-
-
-}
+    console.log(day.weather[0].icon);
+    const cardEL = document.createElement("div");
+    const cardBodyEl = document.createElement("div");
+    const cardTitleContainer = document.createElement("div")
+    const cardTitleEl = document.createElement("h4");
+    const cardTextEl = document.createElement("p");
+    const cardIcon = document.createElement("img");
+    const formattedDate = moment.unix(day.dt).format("MM-DD-YYYY");
+    cardIcon.src = (iconUrl + day.weather[0].icon + iconEnding);
+    cardEL.classList.add("card");
+    cardBodyEl.classList.add("card-body");
+    cardTitleEl.classList.add("card-title");
+    cardTextEl.classList.add("card-text");
+    forecastContainerEl.appendChild(cardEL);
+    cardEL.appendChild(cardBodyEl);
+    cardBodyEl.appendChild(cardTitleContainer);
+    cardTitleContainer.appendChild(cardTitleEl)
+    cardBodyEl.appendChild(cardIcon)
+    cardBodyEl.appendChild(cardTextEl);
+    cardTitleEl.textContent = formattedDate;
+};
 
 
 searchBtn.on("click", search) 
